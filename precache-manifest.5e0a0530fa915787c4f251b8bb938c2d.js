23f266ba4bc124bbe817effc852d72e70d3761c6
johnshelbee <70379175+johnshelbee@users.noreply.github.com>
2020-08-28 15:58:51 +0300

    Add files via upload

---
+++ precache-manifest.5e0a0530fa915787c4f251b8bb938c2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77ea1301bc4d809b3bbb881e8fba61b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62c353799a9450991ae5",
+    "url": "/static/css/main.59d9b7c3.chunk.css"
+  },
+  {
+    "revision": "c48325a3856a320f091a",
+    "url": "/static/js/2.5b0cc486.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.5b0cc486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c353799a9450991ae5",
+    "url": "/static/js/main.dd710c8a.chunk.js"
+  },
+  {
+    "revision": "701d0c9c6b308af07272",
+    "url": "/static/js/runtime-main.69f258bc.js"
+  },
+  {
+    "revision": "c3aece8dcd3a7ebbff39a122335ac4d9",
+    "url": "/static/media/profile_icon.c3aece8d.svg"
+  }
+]);
